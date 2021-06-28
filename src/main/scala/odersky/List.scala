731d6bc03ae7276df19657652a49e16b28c5e820
Witold Kaczurba <witold.kaczurba@gmail.com>
2021-06-28 07:27:32 +0200

    LIst1

--- src/main/scala/odersky/List.scala
+++ src/main/scala/odersky/List.scala
@@ -1,57 +1,67 @@
 package odersky
 
 import java.util.NoSuchElementException
-// ch22.
 
-// Two subclasses: Nil and ::
+  abstract class List[+T] { // +T: covariant, ch 19
+    def isEmpty: Boolean
+    def head: T
+    def tail: List[T]
+    def length: Int = if (isEmpty) 0 else 1 + tail.length
 
-abstract class List[+T] {  // +T: covariant, ch 19
-  def isEmpty : Boolean
-  def head: T
-  def tail: List[T]
+    def drop(n: Int): List[T] =
+      if (isEmpty) Nil
+      else if (n <= 0) this
+      else tail.drop(n - 1)
 
-  def length : Int = if (isEmpty) 0 else 1 + tail.length
-
-  def drop(n : Int) : List[T] =
-    if (isEmpty) Nil
-    else if (n <= 0) this
-    else tail.drop(n - 1)
-}
-
-// Nil object: extends >>Nothing<<
-case object  Nil extends List[Nothing] {
-  override def isEmpty = true
-  def head: Nothing =
-    throw new NoSuchElementException("head of empty list")
-  def tail: Nothing =
-    throw new NoSuchElementException("tail of empty list")
-}
-
-// infix operator - chapter 16.
-// the pattern x :: xs is treated as ::(x, xs)
-
-// THis one can be done simplier - reuse of args:
-//final case class ::[T](hd : T, tl: List[T]) extends List[T] {
-//  def head = hd
-//  def tail = tl
-//  override def isEmpty : Boolean = false
-//}
-
-final case class ::[T](head : T, tail: List[T]) extends List[T] {
-  override def isEmpty : Boolean = false
-}
-
-
-object Main {
-  def main(args : Array[String]): Unit = {
-    val list1 : List[Int] = ::(-1, ::(-2, ::(-3, ::(-f4, Nil))))
-    //val list2 : List[Int] = 1 :: 2 :: 3 :: Nil
-
-    val list2 = list1.drop(2)
-
-    println(list1)
-    println("After drop(2):")
-    println(list2)
+    // This allows to handle different types in list; once a new element of different type is appended - a common supertype will be used.
+    // def ::[U >: T](x : U): List[U] = new scala.::(x, this)
+    def ::[U >: T](x: U): List[U] = {
+      print("Magic happening; we return list casted to a supertype")
+      new ::(x, this)
+    }
+      // NOTE: param U >: T means U a supertype of list element T; we taken U, a supertype of T and return U-typpe.
+      // this way we create a new list of common super type.
 
   }
-}
+
+  // Nil object: extends >>Nothing<<
+  case object Nil extends List[Nothing] {
+    override def isEmpty = true
+
+    def head: Nothing =
+      throw new NoSuchElementException("head of empty list")
+
+    def tail: Nothing =
+      throw new NoSuchElementException("tail of empty list")
+  }
+
+  final case class ::[T](head: T, tail: List[T]) extends List[T] { // head and tail are args name of super-constructor, so they will be assigned to the superconstructor
+    override def isEmpty: Boolean = false
+  }
+
+  object Main {
+    def main(args: Array[String]): Unit = {
+      val list1: List[Int] = ::(-1, ::(-2, ::(-3, ::(-4, Nil))))
+      //val list2 : List[Int] = 1 :: 2 :: 3 :: Nil
+
+      val list2 = list1.drop(2)
+
+      println(list1)
+      println("After drop(2):")
+      println(list2)
+
+    }
+  }
+
+  object ch22p22p2 {
+    def main(args: Array[String]): Unit = {
+      abstract class Fruit
+      class Apple extends Fruit
+      class Orange extends Fruit
+
+      val apples = new Apple :: Nil
+      val fruits : List[Fruit] = new Orange :: apples
+
+      println(fruits)
+    }
+  }
---
+++ src/main/scala/odersky/Notes.md
@@ -0,0 +1,10 @@
+  // infix operator - chapter 16.
+  // the pattern x :: xs is treated as ::(x, xs)
+
+
+
+  // TODO: Look into ::: vs :: operators, eg.: chapter 16, and https://www.baeldung.com/scala/operators-intro
+  //  -> :: and ::: are bound to their right operand
+  //  -> so x :: xs is treated as xs.::(x) operator and not x.::(xs)
+  
+  
